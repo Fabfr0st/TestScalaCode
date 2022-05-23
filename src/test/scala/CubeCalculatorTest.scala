2cef1c3ee0b96ccec70917d9c43635f675c9a830
FabienCeppic <flast.ceppic.cdi22@gmail.com>
2022-05-23 11:54:26 +0200

    test cube with 0

--- src/test/scala/CubeCalculatorTest.scala
+++ src/test/scala/CubeCalculatorTest.scala
@@ -3,5 +3,6 @@ import org.scalatest.funsuite.AnyFunSuite
 class CubeCalculatorTest extends AnyFunSuite {
   test("CubeCalculator.cube") {
     assert(CubeCalculator.cube(3) === 27)
+    assert(CubeCalculator.cube(0) === 0)
   }
 }
